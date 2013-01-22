2c989e9d848060d07b24a6d57948a47d310a258a
Christian Rondeau <christian.rondeau@mediaclip.ca>
2013-01-22 17:25:59 -0500

    Add a test to ensure the same instance is injected to all children

--- tests/spec/src/diSpec.js
+++ tests/spec/src/diSpec.js
@@ -80,5 +80,30 @@ describe("kernel", function () {
             expect(result.outer.inner).toBe(targetFinal);
         });
 
+        it("injects the same instance when many children map to the same dependency", function () {
+            var times = 0;
+            var parent = {
+                child1: undefined,
+                child2: undefined
+            };
+            var injected = {};
+            kernel.set("child1", {
+                property: undefined
+            });
+            kernel.set("child2", {
+                property: undefined
+            });
+            kernel.set("property", function () {
+                times++;
+                return injected;
+            });
+
+            var result = kernel.inject(parent);
+
+            expect(times).toBe(1);
+            expect(result.child1.property).toBe(injected);
+            expect(result.child2.property).toBe(injected);
+        });
+
     });
 });
