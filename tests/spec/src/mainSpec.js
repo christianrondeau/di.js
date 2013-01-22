92e804911a8fd583665703ef0443086ea7e0e3b0
Christian Rondeau <christian.rondeau@mediaclip.ca>
2013-01-21 21:14:05 -0500

    Initial commit with HTML5 Boilerplate and Jasmine

---
+++ tests/spec/src/mainSpec.js
@@ -0,0 +1,14 @@
+﻿describe("Test", function () {
+    var container, a, b;
+
+    beforeEach(function () {
+        container = {};
+        a = {};
+        b = {};
+    });
+
+    it("first test", function () {
+
+        expect(true).toBe(true);
+    });
+});
